b022fd36e97724523a2df328524d61035b8585d5
Bungoh <bungohdev@gmail.com>
2021-06-14 12:03:15 -0400

    Added JavaDocs to Chat.java

--- src/main/java/com/bungoh/bungohutils/chat/Chat.java
+++ src/main/java/com/bungoh/bungohutils/chat/Chat.java
@@ -4,6 +4,11 @@ import org.bukkit.ChatColor;
 
 public class Chat {
 
+    /**
+     * Format a message with basic Spigot chat colors using the '&' character.
+     * @param message The message to be color coded.
+     * @return A formatted, colored String.
+     */
     public static String format(String message) {
         return ChatColor.translateAlternateColorCodes('&', message);
     }
