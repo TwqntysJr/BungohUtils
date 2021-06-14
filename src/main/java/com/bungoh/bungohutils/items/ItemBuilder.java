08f1aa1dba81701e853e761ab73123aa2a5d884d
Bungoh <bungohdev@gmail.com>
2021-06-14 12:02:20 -0400

    Added JavaDocs to ItemBuilder.java

--- src/main/java/com/bungoh/bungohutils/items/ItemBuilder.java
+++ src/main/java/com/bungoh/bungohutils/items/ItemBuilder.java
@@ -13,26 +13,49 @@ public class ItemBuilder {
     private final ItemStack item;
     private final ItemMeta meta;
 
+    /**
+     * Creates an ItemBuilder to be used to create a custom ItemStack.
+     * @param material The material of the ItemStack.
+     */
     public ItemBuilder(Material material) {
         item = new ItemStack(material);
         meta = item.getItemMeta();
     }
 
+    /**
+     * Sets the display name of the ItemStack with chat formatting.
+     * @param name The display name of the ItemStack.
+     * @return The ItemBuilder for further modification.
+     */
     public ItemBuilder setName(String name) {
         meta.setDisplayName(Chat.format(name));
         return this;
     }
 
+    /**
+     * Sets the lore of the ItemStack with chat formatting.
+     * @param lore The variable length String array of which will be the lore of the ItemStack.
+     * @return The ItemBuilder for further modification.
+     */
     public ItemBuilder setLore(String... lore) {
         meta.setLore(Arrays.stream(lore).map(Chat::format).collect(Collectors.toList()));
         return this;
     }
 
+    /**
+     * Sets the quantity of which the ItemStack contains.
+     * @param amount The amount of items for this ItemStack.
+     * @return The ItemBuilder for further modification.
+     */
     public ItemBuilder setAmount(int amount) {
         item.setAmount(amount);
         return this;
     }
 
+    /**
+     * Builds the ItemStack from the constituents of the ItemBuilder.
+     * @return The ItemStack created by the ItemBuilder.
+     */
     public ItemStack build() {
         item.setItemMeta(meta);
         return item;
